f6f7bbe754648451f303b6263f5864d3f2dab3a8
gabrielinuz <gabrielinuz@gmail.com>
2024-06-28 19:12:57 -0300

    a ver si compila

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-g++ -Wall -std=c++14 -I.\include .\lib\sqlite3.dll main.cpp -o .\build\ms_windows\dbh_example.exe
+REM g++ -Wall -std=c++14 -I.\include .\lib\sqlite3.dll main.cpp -o .\build\ms_windows\dbh_example.exe
+g++ -o .\build\ms_windows\dbh_example.exe main.cpp .\lib\sqlite3.dll -I.\include
--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-g++ -Wall -std=c++14 -I./include ./lib/libsqlite3.so main.cpp -o ./build/gnu_linux/dbh_example.bin
+#g++ -Wall -std=c++14 -I./include ./lib/libsqlite3.so main.cpp -o ./build/gnu_linux/dbh_example.bin
+#g++ -Wall -std=c++14 -I./include ./lib/libsqlite3.so.0.8.6 main.cpp -o ./build/gnu_linux/dbh_example.bin
+g++ -o ./build/gnu_linux/dbh_example.bin main.cpp ./lib/libsqlite3.so.0.8.6 -I./include
 
 
 
