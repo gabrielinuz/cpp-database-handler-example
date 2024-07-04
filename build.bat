8e1422d11b1c270fe2ea50cf748f227203e0d446
gabrielinuz <gabrielinuz@gmail.com>
2024-07-04 20:35:49 -0300

    compilar en windows

--- build.bat
+++ build.bat
@@ -1,2 +1,1 @@
-REM g++ -Wall -std=c++14 -I.\include .\lib\sqlite3.dll main.cpp -o .\build\ms_windows\dbh_example.exe
-g++ -o .\build\ms_windows\dbh_example.exe main.cpp .\lib\sqlite3.dll -I.\include
+g++ -Wall -std=c++14 -I include -o build\ms_windows\dbh_example.exe main.cpp -Llib -lsqlite3
